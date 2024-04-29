99197e8374c48a5b7043a6efa5edf58d002fd2f2
Marina Escriva <marinaescriva.24@gmail.com>
2024-04-29 15:06:44 +0200

    feat: server is healthy working

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 
 // import { AppDataSource } from "./database/db";
-// import 'dotenv/config';
-import express from 'express';
 
-// const PORT = process.env.PORT || 4000;
+import express, { Application } from "express";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
 
-app.listen(4001, () => {
-    console.log("server is running");
+const app : Application = express();
+
+const PORT = process.env.PORT || 4001;
+
+app.get('/healthy', (req, res) => {
+    res.send("server is healthy")
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on port: ${PORT}`);
 })
 
 // const startServer = () => {
